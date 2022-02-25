26665dc9891c74cfb222b7ad7c4b696edee5e1f7
12942 <12942@LAPTOP-NLKQLU18>
2022-02-25 18:03:29 +0800

    添加项目文件。

---
+++ W1HW3App/LeapYear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3App
+{
+    internal class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("enter year:");
+            if (int.TryParse(Console.ReadLine(), out year))
+            {
+                if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+                    Console.WriteLine("it is  a Leap");
+                
+                else
+                    Console.WriteLine("it is not a Leap");
+            }
+            else
+                Console.WriteLine("input error,try again");
+
+            Console.ReadLine();
+
+        }
+    }
+}
